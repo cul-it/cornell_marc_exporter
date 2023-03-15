a54ab7120333f4aa555405d1dadbafa8b8586d5b
rtwilson <rtwilson@cornell.edu>
2023-03-15 13:32:56 -0500

    546 and 041

--- backend/model/cornell-marc-overrrides.rb
+++ backend/model/cornell-marc-overrrides.rb
@@ -3,7 +3,6 @@ class MARCModel < ASpaceExport::ExportModel
 
   include JSONModel
 
-
 @resource_map = {
     [:id_0, :id_1, :id_2, :id_3] => :handle_id,
     :notes => :handle_notes,
@@ -13,8 +12,11 @@ class MARCModel < ASpaceExport::ExportModel
     [:finding_aid_status,:ead_id] => :handle_ead_loc
 }
 
+# ANW-1416: Maps ISO-3166 country code to MARC country code
+  ISO_3166_TO_MARC = {"AE" => "ts", "AF" => "af", "AG" => "aq", "AI" => "ag", "AL" => "aa", "AM" => "ai", "AO" => "ao", "AQ" => "ay", "AR" => "ag", "AS" => "as", "AT" => "au", "AU" => "at", "AW" => "aw", "AX" => "xx", "AZ" => "aj", "BA" => "bn", "BB" => "bb", "BD" => "bg", "BE" => "be", "BF" => "xx", "BG" => "bu", "BH" => "ba", "BI" => "bd", "BJ" => "dm", "BL" => "sc", "BM" => "bm", "BN" => "bx", "BO" => "bo", "BQ" => "xx", "BR" => "bl", "BS" => "bf", "BT" => "bt", "BV" => "bv", "BW" => "bs", "BY" => "bw", "BZ" => "bh", "CA" => "xxc", "CC" => "xb", "CD" => "cg", "CF" => "cx", "CG" => "cf", "CH" => "sz", "CI" => "iv", "CK" => "cw", "CL" => "cl", "CM" => "cm", "CN" => "cc", "CO" => "ck", "CR" => "cr", "CU" => "cu", "CV" => "cv", "CW" => "co", "CX" => "xa", "CY" => "cy", "CZ" => "xr", "DE" => "gw", "DJ" => "ft", "DK" => "dk", "DM" => "dq", "DO" => "dr", "DZ" => "ae", "EC" => "ec", "EE" => "er", "EG" => "ua", "EH" => "ss", "ER" => "ea", "ES" => "sp", "ET" => "et", "FI" => "fi", "FJ" => "fj", "FK" => "fk", "FM" => "fm", "FO" => "fa", "FR" => "fr", "GA" => "go", "GB" => "xxk", "GD" => "gd", "GE" => "gs", "GF" => "gv", "GG" => "gg", "GH" => "gh", "GI" => "gi", "GL" => "gl", "GM" => "gm", "GN" => "gv", "GP" => "gp", "GQ" => "eg", "GR" => "gr", "GS" => "xs", "GT" => "gt", "GU" => "gu", "GW" => "pg", "GY" => "gy", "HK" => "xx", "HM" => "hm", "HN" => "ho", "HR" => "ci", "HT" => "ht", "HU" => "hu", "ID" => "io", "IE" => "ie", "IL" => "is", "IM" => "im", "IN" => "ii", "IO" => "bi", "IQ" => "iq", "IR" => "ir", "IS" => "ic", "IT" => "it", "JE" => "je", "JM" => "jm", "JO" => "jo", "JP" => "ja", "KE" => "ke", "KG" => "kg", "KH" => "cb", "KI" => "gb", "KM" => "cq", "KN" => "xd", "KP" => "kn", "KR" => "ko", "KW" => "ku", "KY" => "cj", "KZ" => "kz", "LA" => "xx", "LB" => "le", "LC" => "xk", "LI" => "lh", "LK" => "ce", "LR" => "lb", "LS" => "lo", "LT" => "li", "LU" => "lu", "LV" => "lv", "LY" => "ly", "MA" => "mr", "MC" => "mc", "MD" => "mv", "ME" => "mo", "MF" => "st", "MG" => "mg", "MH" => "xe", "MK" => "xn", "ML" => "ml", "MM" => "br", "MN" => "mp", "MO" => "xx", "MP" => "nw", "MQ" => "mq", "MR" => "mu", "MS" => "mj", "MT" => "mm", "MU" => "mf", "MV" => "xc", "MW" => "mw", "MX" => "mx", "MY" => "my", "MZ" => "mz", "NA" => "sx", "NC" => "nl", "NE" => "ng", "NF" => "nx", "NG" => "nr", "NI" => "nq", "NL" => "ne", "NO" => "no", "NP" => "np", "NR" => "nu", "NU" => "xh", "NZ" => "nz", "OM" => "mk", "PA" => "pn", "PE" => "pe", "PF" => "fp", "PG" => "pp", "PH" => "ph", "PK" => "pk", "PL" => "pl", "PM" => "xl", "PN" => "pc", "PR" => "pr", "PS" => "xx", "PT" => "po", "PW" => "pw", "PY" => "py", "QA" => "qa", "RE" => "re", "RO" => "rm", "RS" => "rb", "RU" => "ru", "RW" => "rw", "SA" => "su", "SB" => "bp", "SC" => "se", "SD" => "sj", "SE" => "sw", "SG" => "si", "SH" => "xj", "SI" => "xv", "SJ" => "xx", "SK" => "xo", "SL" => "si", "SM" => "sm", "SN" => "sg", "SO" => "so", "SR" => "sr", "SS" => "sd", "ST" => "sf", "SV" => "es", "SX" => "sn", "SY" => "sy", "SZ" => "xx", "TC" => "tc", "TD" => "cd", "TF" => "xx", "TG" => "tg", "TH" => "th", "TJ" => "ta", "TK" => "tl", "TL" => "em", "TM" => "tk", "TN" => "ti", "TO" => "to", "TR" => "tu", "TT" => "tr", "TV" => "tv", "TW" => "xx", "TZ" => "tz", "UA" => "un", "UG" => "ug", "UM" => "xxu", "US" => "xxu", "UY" => "uy", "UZ" => "uz", "VA" => "vc", "VC" => "xm", "VE" => "ve", "VG" => "vb", "VI" => "vi", "VN" => "vm", "VU" => "nn", "WF" => "wf", "WS" => "ws", "YE" => "ye", "YT" => "ot", "ZA" => "sa", "ZM" => "za", "ZW" => "rh"}
 
-  def self.from_resource(obj, opts={})
+
+def self.from_resource(obj, opts={})
     marc = self.from_archival_object(obj,opts)
     marc.apply_map(obj, @resource_map)
     marc.leader_string = "00000cp$aa2200000 a 4500"
@@ -25,27 +27,34 @@ class MARCModel < ASpaceExport::ExportModel
     marc
   end
 
+
   def self.assemble_controlfield_string(obj)
     date = obj.dates[0] || {}
     string = obj['system_mtime'].scan(/\d{2}/)[1..3].join('')
     string += date['date_type'] == 'single' ? 's' : 'i'
     string += date['begin'] ? date['begin'][0..3] : "    "
     string += date['end'] ? date['end'][0..3] : "    "
-    string += "nyu"
+    string += "nyu"	
+
+    repo = obj['repository']['_resolved']
+
+#    if repo.has_key?('country') && !repo['country'].empty?
+#      string += (ISO_3166_TO_MARC[repo['country']] || "xx")
+#    else
+#      string += "xx"
+#    end
+
     # If only one Language and Script subrecord its code value should be exported in the MARC 008 field position 35-37; If more than one Language and Script subrecord is recorded, a value of "mul" should be exported in the MARC 008 field position 35-37.
     lang_materials = obj.lang_materials
     languages = lang_materials.map {|l| l['language_and_script']}.compact
-    langcode = languages.count == 1 ? languages[0]['language'] : 'eng'
-
-    # variable number of spaces needed since country code could have 2 or 3 chars
-    (35-(string.length)).times { string += ' ' }
-    string += (langcode || '|||')
-    string += ' d'
-
-    string
+    langcode = languages.count == 1 ? languages[0]['language'] : 'mul'    
+    (35-(string.length)).times { string += ' ' }	
+    string += (langcode || '|||')	
+    string += ' d'	
+     string	
   end
 
-  def handle_ead_loc(finding_aid_status,ead_id)
+def handle_ead_loc(finding_aid_status,ead_id)
     if ead_id && !ead_id.empty?
       if finding_aid_status && finding_aid_status == "completed"
       df('856', '4', '2').with_sfs(
@@ -56,17 +65,8 @@ class MARCModel < ASpaceExport::ExportModel
     end
   end
 
-
-
-  def handle_repo_code(repository,langcode)
-  
-    df('040', ' ', ' ').with_sfs(['a', 'NIC'], ['b', 'eng'],['c', 'NIC'])
-
-  end
-
-##currently also blocking 546
-def handle_languages(lang_materials)
-  #blocks output of 041
+def handle_repo_code(repository,langcode)
+  df('040', ' ', ' ').with_sfs(['a', 'NIC'], ['b', 'eng'],['c', 'NIC'])
 end
 
 def handle_voyager_id(id_0)
@@ -77,294 +77,8 @@ def handle_ref(id)
   df('035', ' ', ' ').with_sfs(['a',"(CULAspaceURI)" + id.to_s])
 end
 
-def handle_extents(extents)
-  extents.each do |ext|
-    e = ext['number']
-    t =  "#{I18n.t('enumerations.extent_extent_type.'+ext['extent_type'], :default => ext['extent_type'])}"
-
-    
-
-    df!('300').with_sfs(['a', e], ['f', t])
-  end
+def handle_language(langcode)
+  #blocks output of 041
 end
-
-
-def handle_id(*ids)
-  ids.reject!{|i| i.nil? || i.empty?}
-  df('099', ' ', '9').with_sfs(['a', ids.join('.')])
- 
-end
-
-def handle_notes(notes)
-
-  notes.each do |note|
-    if note['type'] == 'scopecontent'
-      text = ASpaceExport::Utils.extract_note_text(note, @include_unpublished, true) 
-      if text.length > 0 
-        texts = []
-        texts = text.split("\n")
-        n = 0
-        texts.each do |t|
-          n = n+1
-          if t.length > 0
-            if n == 1
-              df!('520', '2', ' ').with_sfs(['a', *Array(t)])
-            else
-              df!('520', '8', ' ').with_sfs(['a', *Array(t)])
-            end
-          end
-      end
-    end
-
-  else
-
-    prefix =  case note['type']
-              when 'dimensions'; "Dimensions"
-              when 'physdesc'; "Physical Description note"
-              when 'materialspec'; "Material Specific Details"
-              when 'physloc'; "Location of resource"
-              when 'phystech'; "Physical Characteristics / Technical Requirements"
-              when 'physfacet'; "Physical Facet"
-              when 'processinfo'; "Processing Information"
-              when 'separatedmaterial'; "Materials Separated from the Resource"
-              else; nil
-              end
-
-    marc_args = case note['type']
-
-                when 'arrangement', 'fileplan'
-                  ['351', 'a']
-                when 'odd', 'dimensions', 'physdesc', 'materialspec', 'physloc', 'phystech', 'physfacet', 'processinfo', 'separatedmaterial'
-                  ['500','a']
-                when 'accessrestrict'
-                  ['506','a']
-               
-                when 'abstract'
-                  ['520', '3', ' ', 'a']
-                when 'prefercite'
-                  ['524', ' ', ' ', 'a']
-                when 'acqinfo'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['541', ind1, ' ', 'a']
-                when 'relatedmaterial'
-                  ['544','d']
-                when 'bioghist'
-                  ['545','a']
-                when 'custodhist'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['561', ind1, ' ', 'a']
-                when 'appraisal'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['583', ind1, ' ', 'a']
-                when 'accruals'
-                  ['584', 'a']
-                when 'altformavail'
-                  ['535', '2', ' ', 'a']
-                when 'originalsloc'
-                  ['535', '1', ' ', 'a']
-                when 'userestrict', 'legalstatus'
-                  ['540', 'a']
-                when 'langmaterial'
-                  ['546', 'a']
-                when 'otherfindaid'
-                  ['555', '0', ' ', 'a']
-                else
-                  nil
-                end
-
-    unless marc_args.nil?
-      text = prefix ? "#{prefix}: " : ""
-      text += ASpaceExport::Utils.extract_note_text(note, @include_unpublished, true) 
-
-      # only create a tag if there is text to show (e.g., marked published or exporting unpublished)
-      if text.length > 0 
-        df!(*marc_args[0...-1]).with_sfs([marc_args.last, *Array(text)])
-      end
-    end
-  end
-end
-end
-
-def handle_notes(notes)
-
-  notes.each do |note|
-    if note['type'] == 'scopecontent'
-      text = ASpaceExport::Utils.extract_note_text(note, @include_unpublished, true) 
-      if text.length > 0 
-        texts = []
-        texts = text.split("\n")
-        n = 0
-        texts.each do |t|
-          n = n+1
-          if t.length > 0
-            if n == 1
-              df!('520', '2', ' ').with_sfs(['a', *Array(t)])
-            else
-              df!('520', '8', ' ').with_sfs(['a', *Array(t)])
-            end
-          end
-      end
-    end
-
-  else
-
-    prefix =  case note['type']
-              when 'dimensions'; "Dimensions"
-              when 'physdesc'; "Physical Description note"
-              when 'materialspec'; "Material Specific Details"
-              when 'physloc'; "Location of resource"
-              when 'phystech'; "Physical Characteristics / Technical Requirements"
-              when 'physfacet'; "Physical Facet"
-              when 'processinfo'; "Processing Information"
-              when 'separatedmaterial'; "Materials Separated from the Resource"
-              else; nil
-              end
-
-    marc_args = case note['type']
-
-                when 'arrangement', 'fileplan'
-                  ['351', 'a']
-                when 'odd', 'dimensions', 'physdesc', 'materialspec', 'physloc', 'phystech', 'physfacet', 'processinfo', 'separatedmaterial'
-                  ['500','a']
-                when 'accessrestrict'
-                  ['506','a']
-               
-                when 'abstract'
-                  ['520', '3', ' ', 'a']
-                when 'prefercite'
-                  ['524', ' ', ' ', 'a']
-                when 'acqinfo'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['541', ind1, ' ', 'a']
-                when 'relatedmaterial'
-                  ['544','d']
-                when 'bioghist'
-                  ['545','a']
-                when 'custodhist'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['561', ind1, ' ', 'a']
-                when 'appraisal'
-                  ind1 = note['publish'] ? '1' : '0'
-                  ['583', ind1, ' ', 'a']
-                when 'accruals'
-                  ['584', 'a']
-                when 'altformavail'
-                  ['535', '2', ' ', 'a']
-                when 'originalsloc'
-                  ['535', '1', ' ', 'a']
-                when 'userestrict', 'legalstatus'
-                  ['540', 'a']
-                when 'langmaterial'
-                  ['546', 'a']
-                when 'otherfindaid'
-                  ['555', '0', ' ', 'a']
-                else
-                  nil
-                end
-
-    unless marc_args.nil?
-      text = prefix ? "#{prefix}: " : ""
-      text += ASpaceExport::Utils.extract_note_text(note, @include_unpublished, true) 
-
-      # only create a tag if there is text to show (e.g., marked published or exporting unpublished)
-      if text.length > 0 
-        df!(*marc_args[0...-1]).with_sfs([marc_args.last, *Array(text)])
-      end
-    end
-  end
-end
-end
-
-  def handle_primary_creator(linked_agents)
-    link = linked_agents.find {|a| a['role'] == 'creator'}
-    return nil unless link
-    return nil unless link["_resolved"]["publish"] || @include_unpublished
-
-    creator = link['_resolved']
-    name = creator['display_name']
-
-    ind2 = ' '
-
-    relator_sfs = []
-    if link['relator']
-      handle_relators(relator_sfs, link['relator'])
-    else
-      relator_sfs << ['e', 'creator']
-    end
-
-    case creator['agent_type']
-
-    when 'agent_corporate_entity'
-      code = '110'
-      ind1 = '2'
-      sfs = gather_agent_corporate_subfield_mappings(name, relator_sfs, creator)
-
-    when 'agent_person'
-      ind1  = name['name_order'] == 'direct' ? '0' : '1'
-      code = '100'
-      sfs = gather_agent_person_subfield_mappings(name, relator_sfs, creator)
-
-    when 'agent_family'
-      code = '100'
-      ind1 = '3'
-      sfs = gather_agent_family_subfield_mappings(name, relator_sfs, creator)
-
-    end
-
-    df(code, ind1, ind2).with_sfs(*sfs)
-  end
-
-  # TODO: DRY this up
-  # this method is very similair to handle_primary_creator and handle_agents
-  def handle_other_creators(linked_agents)
-    creators = linked_agents.select {|a| a['role'] == 'creator'}[1..-1] || []
-    creators = creators + linked_agents.select {|a| a['role'] == 'source'}
-
-    creators.each_with_index do |link, i|
-      next unless link["_resolved"]["publish"] || @include_unpublished
-
-      creator = link['_resolved']
-      name = creator['display_name']
-      role = link['role']
-
-      relator_sfs = []
-      if link['relator']
-        handle_relators(relator_sfs, link['relator'])
-      elsif role == 'source'
-        relator_sfs << ['e', 'former owner']
-      else
-        relator_sfs << ['e', 'creator']
-      end
-
-      ind2 = ' '
-
-      case creator['agent_type']
-
-      when 'agent_corporate_entity'
-        code = '710'
-        ind1 = '2'
-        sfs = gather_agent_corporate_subfield_mappings(name, relator_sfs, creator)
-
-      when 'agent_person'
-        ind1  = name['name_order'] == 'direct' ? '0' : '1'
-        code = '700'
-        sfs = gather_agent_person_subfield_mappings(name, relator_sfs, creator)
-
-      when 'agent_family'
-        ind1 = '3'
-        code = '700'
-        sfs = gather_agent_family_subfield_mappings(name, relator_sfs, creator)
-
-      end
-
-      df(code, ind1, ind2, i).with_sfs(*sfs)
-    end
-  end
-
-
-
-
-
-
 
 end
